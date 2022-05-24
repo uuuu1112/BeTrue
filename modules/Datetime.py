fc324248a7adac68a2d6caa3a1549fac81ba84b7
Jesse Tseng <pkh_uuuu1112@photons.com.tw>
2022-05-24 18:14:31 +0800

    fix time module

--- modules/Datetime.py
+++ modules/Datetime.py
@@ -4,3 +4,11 @@ def beforeDays(days):
     today=datetime.date.today()
     beforeDays=datetime.timedelta(days=days)
     return today-beforeDays
+
+def beforeMonthss(months):
+    today=datetime.date.today()
+    beforeMonths=datetime.timedelta(monthss=months)
+    return today-beforeMonths
+
+def test():
+    return "test"
